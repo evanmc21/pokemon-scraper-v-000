8de330054bf5dfc27dec58ce24f4fea9c55154f5
Evan Candler <emcandler21@gmail.com>
2018-03-30 18:27:39 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,12 +11,16 @@ class Pokemon
     @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type) #use bound parameters for any variable data that you are passing into your SQL statement
   end
 
   def self.find(id, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
-    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db, hp: pokemon[3])
   end
 end
